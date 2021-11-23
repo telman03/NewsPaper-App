66a3387acc0bf1f00befff2b62bdbc58a48011c4
telman03 <telmangadimov1@gmail.com>
2021-11-23 10:38:21 +0400

    initial commit

--- articles/views.py
+++ articles/views.py
@@ -1,29 +1,43 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User # new
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView # new
 from django.views.generic.edit import UpdateView, DeleteView, CreateView # new
 from django.urls import reverse_lazy # new
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
-class ArticleDetailView(DetailView): # new
+class ArticleDetailView(LoginRequiredMixin, DetailView): # new
     model = Article
     template_name = 'article_detail.html'
 
-class ArticleUpdateView(UpdateView): # new
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView): # new
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
-class ArticleDeleteView(DeleteView): # new
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView): # new
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-class ArticleCreateView(CreateView): # new
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView): # new
     model = Article
     template_name = 'article_new.html'
     fields = ('title', 'body', 'author')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8f9_6sc7^hn+2j!7l^(v!68_a%axll3$h212f*la213g6$qen7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic', # new
     'django.contrib.staticfiles',
 
     # 3rd party
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] # new
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
